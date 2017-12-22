9dd7aff20b71dd942a8e03b484aefcffc2efc67b
hamzicabdulah <abdulahhamzic@gmail.com>
2017-12-22 12:40:14 +0100

    Modified package.json scripts

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,10 +7,11 @@ import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import { App } from './components/App';
 import { usersActions } from './actions/usersActions';
 import { rootReducer } from './reducers/rootReducer';
+import { Store } from "redux";
 
 const initialState: any = undefined;
 
-const store = createStore(
+const store: Store<any> = createStore(
   rootReducer,
   initialState,
   applyMiddleware(thunk, reduxImmutableStateInvariant())
